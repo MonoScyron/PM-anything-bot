b4cc95dc622462d1266f45dc47d9daa63dc5cf2c
MonoScyron <110511317+MonoScyron@users.noreply.github.com>
2023-01-12 02:02:28 -0500

    Added new events & events images testing

--- src/testing/test_func.py
+++ src/testing/test_func.py
@@ -3,7 +3,7 @@ import json
 
 def check_images():
     """
-    Checks through pull file and makes sure all images are valid
+    Checks through pull and event lists and makes sure all images are valid
     :return:None
     """
     pull_file = open('../lists/pull_list.json')
@@ -14,6 +14,17 @@ def check_images():
         curr_d = pull_list[k]
         for curr_k in curr_d.keys():
             open(f'.{curr_d[curr_k]}')
+
+    event_file = open('../lists/event_list.json')
+    event_list = json.load(event_file)
+    event_file.close()
+
+    event_list = event_list['events']
+
+    for curr_d in event_list:
+        if 'event_pic' in curr_d:
+            for k in curr_d['event_pic']:
+                open(f'.{curr_d["event_pic"][k]}')
 
 
 check_images()
