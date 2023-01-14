9a113d09d44c5aa580deb4f73d0b392cd14cf7c3
MonoScyron <110511317+MonoScyron@users.noreply.github.com>
2023-01-14 18:17:49 -0500

    Added events
    - Solos
    - Restraining order
    - Blender
    - Imposter

--- src/testing.py
+++ src/testing.py
@@ -1,6 +1,6 @@
 import json
 
-from func import event_str_parse
+from func import event_str_parse, list_pull
 
 
 def check_images():
@@ -34,13 +34,10 @@ def test_list_pull():
     Pulls events from test_list.json
     :return:None
     """
-    event_file = open('lists/test_event_list.json')
-    event_list = json.load(event_file)
-    event_file.close()
-    event_list = event_list['events']
-
-    for curr_d in event_list:
-        print(event_str_parse.parse_event_text(curr_d['event_text'], char_path='lists/test_pull_list.json'))
+    event_text, event_pics_d = list_pull.pull_event(pull_path='./lists/test_event_list.json')
+    parsed_text, pics = event_str_parse.parse_event(event_text=event_text, event_pics_d=event_pics_d,
+                                                    char_path='./lists/test_pull_list.json')
+    print(f'{parsed_text} - {pics}')
 
 
 check_images()
