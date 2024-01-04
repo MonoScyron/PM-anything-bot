73d52edd3d2901a630a50b76953712a6fc44758e
MonoScyron <110511317+MonoScyron@users.noreply.github.com>
2024-01-04 12:57:45 -0500

    fix issue with .nDef interaction with other modifiers

--- src/verify.py
+++ src/verify.py
@@ -1,4 +1,6 @@
 import json
+import re
+import sys
 
 from eventparser import EventParser
 
@@ -11,6 +13,10 @@ json_file.close()
 for k in trace_obj.keys():
     event_list = trace_obj[k]
     for event in event_list:
+        if re.search(r'\#\w+\.\w+\.nDef\#', event):
+            sys.stdout.write(f'\033[91m"{event}" has invalid use of nDef\033[0m')
+            exit(1)
+
         raw_text = parser.flatten(event)
         text, pics = parser.parse_raw_text(raw_text)
 
