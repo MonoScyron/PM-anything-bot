57777d3281f4319bcf9f1300a2b6576e1511fca9
MonoScyron <110511317+MonoScyron@users.noreply.github.com>
2024-01-05 18:02:38 -0500

    add chars, events, fix verify.py

--- src/verify.py
+++ src/verify.py
@@ -1,4 +1,5 @@
 import json
+import os.path
 import re
 import sys
 
@@ -14,14 +15,15 @@ for k in trace_obj.keys():
     event_list = trace_obj[k]
     for event in event_list:
         if re.search(r'\#\w+\.\w+\.nDef\#', event):
-            sys.stdout.write(f'\033[91m"{event}" has invalid use of nDef\033[0m')
+            sys.stderr.write(f'"{event}" has invalid use of nDef')
             exit(1)
 
         raw_text = parser.flatten(event)
         text, pics = parser.parse_raw_text(raw_text)
 
         for pic in pics:
-            p = open(pic)
-            p.close()
+            if not os.path.exists(pic):
+                sys.stderr.write(f'No such path exists: {pic}')
+                exit(1)
 
         print(f'{event}')
